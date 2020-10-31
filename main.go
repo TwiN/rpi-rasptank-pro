4bb0b902ab2a9cf66f184613435d5c8eb30c9824
TwinProduction <twin@twinnation.org>
2020-10-31 13:17:42 -0400

    Try mcuadros/go-rpi-ws281x again

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"github.com/TwinProduction/rpi-rasptank-pro/controller"
 	"github.com/TwinProduction/rpi-rasptank-pro/display"
+	"github.com/mcuadros/go-rpi-ws281x"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
+	"image"
+	"image/color"
+	"image/draw"
 	"log"
 	"time"
 )
@@ -36,18 +40,31 @@ func main() {
 	//	})
 	//}
 	//pca9685 := i2c.NewPCA9685Driver(rpi)
+	ws2812Config := &ws281x.DefaultConfig
+	ws2812Config.Pin = 32
 
 	led := gpio.NewLedDriver(rpi, "32")
 	work := func() {
 		if err := screen.DisplayIP(); err != nil {
 			log.Printf("Failed to write on screen: %s", err.Error())
 		}
-		gobot.Every(2*time.Second, func() {
-			led.Brightness(3)
-			err := led.Toggle()
-			if err != nil {
+		gobot.Every(3*time.Second, func() {
+			c, _ := ws281x.NewCanvas(8, 4, ws2812Config)
+			if err := c.Initialize(); err != nil {
 				log.Println(err)
 			}
+			draw.Draw(c, c.Bounds(), image.NewUniform(color.White), image.ZP, draw.Over)
+			if err := c.Render(); err != nil {
+				log.Println(err)
+			}
+			time.Sleep(time.Second * 1)
+
+			// don't forget close the canvas, if not you leds may remain on
+			c.Close()
+			//err := led.Toggle()
+			//if err != nil {
+			//	log.Println(err)
+			//}
 			//engine.Right()
 		})
 	}
