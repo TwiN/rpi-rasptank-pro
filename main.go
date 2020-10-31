c14c537cd59d6d3abe84faa9e354983d76fbfc6a
TwinProduction <twin@twinnation.org>
2020-10-30 22:11:23 -0400

    Start working on DC motor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/TwinProduction/rpi-rasptank-pro/display"
 	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/i2c"
 	"gobot.io/x/gobot/platforms/raspi"
 	"log"
 	"time"
@@ -29,7 +30,7 @@ func main() {
 	//}
 	//pca9685 := i2c.NewPCA9685Driver(rpi)
 
-	//adaFruit := i2c.NewAdafruitMotorHatDriver(rpi)
+	adaFruit := i2c.NewAdafruitMotorHatDriver(rpi, i2c.WithBus(1))
 	work := func() {
 		err := display.DrawString(screen, fmt.Sprintf("%s", GetLocalIP()))
 		if err != nil {
@@ -38,26 +39,26 @@ func main() {
 		gobot.Every(3*time.Second, func() {
 			//pca9685.SetPWMFreq(50)
 			//pca9685.SetPWM()
-			//log.Println("o.o")
-			//var speed int32 = 10 // 255 = full speed!
-			//if err := adaFruit.SetDCMotorSpeed(2, speed); err != nil {
-			//	log.Println(err)
-			//}
-			//if err := adaFruit.RunDCMotor(2, i2c.AdafruitForward); err != nil {
-			//	log.Println(err)
-			//}
-			//time.Sleep(100 * time.Millisecond)
-			//if err := adaFruit.RunDCMotor(2, i2c.AdafruitRelease); err != nil {
-			//	log.Println(err)
-			//}
-			//time.Sleep(100 * time.Millisecond)
-			//if err := adaFruit.RunDCMotor(2, i2c.AdafruitBackward); err != nil {
-			//	log.Println(err)
-			//}
-			//time.Sleep(100 * time.Millisecond)
-			//if err := adaFruit.RunDCMotor(2, i2c.AdafruitRelease); err != nil {
-			//	log.Println(err)
-			//}
+			log.Println("o.o")
+			var speed int32 = 10 // 255 = full speed!
+			if err := adaFruit.SetDCMotorSpeed(2, speed); err != nil {
+				log.Println(err)
+			}
+			if err := adaFruit.RunDCMotor(2, i2c.AdafruitForward); err != nil {
+				log.Println(err)
+			}
+			time.Sleep(100 * time.Millisecond)
+			if err := adaFruit.RunDCMotor(2, i2c.AdafruitRelease); err != nil {
+				log.Println(err)
+			}
+			time.Sleep(100 * time.Millisecond)
+			if err := adaFruit.RunDCMotor(2, i2c.AdafruitBackward); err != nil {
+				log.Println(err)
+			}
+			time.Sleep(100 * time.Millisecond)
+			if err := adaFruit.RunDCMotor(2, i2c.AdafruitRelease); err != nil {
+				log.Println(err)
+			}
 		})
 	}
 
