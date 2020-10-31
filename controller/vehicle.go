86dc1c5cfc9b5bd13ecc23829a90d096f5cadf49
TwinProduction <twin@twinnation.org>
2020-10-31 16:45:42 -0400

    Print current speed for debugging purposes

--- controller/vehicle.go
+++ controller/vehicle.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 	"log"
@@ -66,4 +67,5 @@ func (v *Vehicle) move(leftMotorDirection, rightMotorDirection string) {
 		log.Printf("Stopping because failed to send direction to right motor: %s", err.Error())
 		v.Stop()
 	}
+	fmt.Printf("current speed: %d and %d\n", v.LeftMotor.CurrentSpeed, v.RightMotor.CurrentSpeed)
 }
